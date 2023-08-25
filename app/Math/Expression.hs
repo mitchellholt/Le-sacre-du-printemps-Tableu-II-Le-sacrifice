cf38828610898f814501451b567ac2dea7a1cfeb
Joel Richardson <joelwrichardson01@gmail.com>
2023-08-25 21:38:20 +1000

    added rewrite rules

--- app/Math/Expression.hs
+++ app/Math/Expression.hs
@@ -14,14 +14,27 @@ data Expr a where
     Not :: Expr Bool -> Expr Bool
     Forall :: Expr Bool -> Expr Bool -- binds
     Exists :: Expr Bool -> Expr Bool -- binds
-    Construct :: Expr Bool -> Expr (Set a) -- binds
-    ElementOf :: Expr a -> Expr (Set a) -> Expr Bool
 
     -- each of the binds increments these fellas,
     -- that way the nat is kinda like the scope
     Variable :: Natural -> Expr a 
 
+    Construct :: Expr Bool -> Expr (Set a) -- binds
+    ElementOf :: Expr a -> Expr (Set a) -> Expr Bool
+
+    Equals :: Expr a -> Expr a -> Expr Bool
+
+    -- not worrying about functions yet, but they might go here.
+
     Lift :: a -> Expr a -- illegal cheese
+
+
+-- not set on these guys tbh - defo need to change.
+data Rewrite a where
+    Rule :: (Expr a -> Expr a) -> Rewrite a
+    Builder :: Rewrite a -> Rewrite b
+
+-- sametobothsides :: (a -> b) -> Rewrite a b
 
 
 -- I'm thinking this is gonna land somewhere logic programmy
