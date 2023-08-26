196c352eb27df9809f840efb8af4d4fdc4c24f10
Mitchell Holt <mitchellholt2002@gmail.com>
2023-08-26 14:03:11 +1000

    did a bit of work on use lol

--- app/Math/Expression.hs
+++ app/Math/Expression.hs
@@ -4,17 +4,19 @@ module Math.Expression where
 
 import Data.Natural
 
-data Set a = Set (a -> Bool)
+newtype Set a = Set (a -> Bool)
 
 -- type (-->) a b = (a,b)
 
+-- a is outsie type, e.g. Equal :: forall b. Expr b Bool
+-- b is the "inside" type, so 5 == 5 :: Expr Int Bool
 data Expr b a where
     And :: Expr b Bool -> Expr c Bool -> Expr (b,c,Bool) Bool
     Or :: Expr b Bool -> Expr c Bool -> Expr (b,c,Bool) Bool
     Not :: Expr b Bool -> Expr (b, Bool) Bool
     Forall :: Expr b Bool -> Expr (b, Bool) Bool -- binds
     Exists :: Expr b Bool -> Expr (b, Bool) Bool -- binds
-    True :: Expr () Bool
+    SoTrue :: Expr () Bool
 
     -- each of the binds increments these fellas,
     -- that way the nat is kinda like the scope
@@ -34,6 +36,27 @@ type (-->) = (,)
 data RR a where
     Rule :: (a -> a) -> RR a
     Builder :: (RR a -> RR b) -> RR (a --> b)
+
+
+-- Apply a rewrite to an expression
+use :: RR a -> Expr b a -> Expr b a
+use rule = \case
+    --And :: Expr b Bool -> Expr c Bool -> Expr (b,c,Bool) Bool
+    And p q -> And (use rule p) (use rule q)
+    -- Or :: Expr b Bool -> Expr c Bool -> Expr (b,c,Bool) Bool
+    Or p q -> Or (use rule p) (use rule q)
+    -- Not :: Expr b Bool -> Expr (b, Bool) Bool
+    Not p -> Not (use rule p)
+    -- Forall :: Expr b Bool -> Expr (b, Bool) Bool -- binds
+    Forall p -> Forall (use rule p)
+    -- Exists :: Expr b Bool -> Expr (b, Bool) Bool -- binds
+    Exists p -> Exists (use rule p)
+    -- SoTrue :: Expr () Bool
+    --Variable :: Natural -> Expr () a 
+    --Construct :: Expr b Bool -> Expr (b, Bool) (Set a) -- binds
+    --ElementOf :: Expr b a -> Expr c (Set a) -> Expr (b, c, Set a) Bool
+    --Equals :: Expr b a -> Expr c a -> Expr (b, c, a) Bool
+    --Lift :: a -> Expr () a -- illegal cheese
 
 
 sametobothsides :: RR (Expr _ a --> Expr (_, _, a) Bool)
