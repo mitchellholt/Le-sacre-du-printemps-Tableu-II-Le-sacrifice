fb3f62a75f3a05d9afaf77109a186dd2aad91e1f
Joel Richardson <joelwrichardson01@gmail.com>
2023-08-26 14:02:38 +1000

    tweaked RR

--- app/Math/Expression.hs
+++ app/Math/Expression.hs
@@ -14,7 +14,7 @@ data Expr b a where
     Not :: Expr b Bool -> Expr (b, Bool) Bool
     Forall :: Expr b Bool -> Expr (b, Bool) Bool -- binds
     Exists :: Expr b Bool -> Expr (b, Bool) Bool -- binds
-    True :: Expr () Bool
+    SoTrue :: Expr () Bool
 
     -- each of the binds increments these fellas,
     -- that way the nat is kinda like the scope
@@ -30,23 +30,43 @@ data Expr b a where
     Lift :: a -> Expr () a -- illegal cheese
 
 
-type (-->) = (,)
+newbind :: Expr b a -> Expr b a
+newbind (Variable n) = Variable (n+1)
+newbind (And a b) = And (newbind a) (newbind b)
+newbind (Or a b) = Or (newbind a) (newbind b)
+newbind (Not a) = Not (newbind a)
+newbind (Forall a) = Forall (newbind a)
+newbind (Exists a) = Exists (newbind a)
+newbind (SoTrue) = SoTrue
+newbind (Construct a) = Construct (newbind a)
+newbind (ElementOf a b) = ElementOf (newbind a) (newbind b)
+newbind (Equals a b) = Equals (newbind a) (newbind b)
+newbind (Lift a) = Lift a
+
+newtype RRtoRR a = RRtoRR a
+type (-->) a b = RRtoRR (a,b)
+newtype AtoA a = AtoA a
+type (-+>) a b = AtoA (a,b)
+type E a = AtoA (a,a)
+
 data RR a where
-    Rule :: (a -> a) -> RR a
+    Rule :: (a -> b) -> RR (a -+> b)
     Builder :: (RR a -> RR b) -> RR (a --> b)
 
 
-sametobothsides :: RR (Expr _ a --> Expr (_, _, a) Bool)
+sametobothsides :: RR ( E (Expr _ a) --> E (Expr (_, _, a) Bool) )
 sametobothsides = Builder 
                     ( \(Rule (f :: Expr _ a -> Expr _ a)) ->
                         Rule ( \case
-                            Equals a b -> Equals (f a) (f b)
-                            _ -> error "must be applied to equality"
-                        )
+                                Equals a b -> Equals (f a) (f b)
+                                _ -> error "must be applied to equality"
+                            )
                     )
 
 
--- introduceForall :: RR (Expr b Bool --> Expr  Bool)
+introduceForall :: RR (Expr b Bool -+> Expr (b, Bool) Bool )
+introduceForall = Rule (Forall . newbind)
+
 
 -- meme = 
 
